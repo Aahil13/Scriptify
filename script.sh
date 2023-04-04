718f796074f870d63e00b67830fb1131f37af4e4
onyeanuna prince eze <onyeanunaprince@gmail.com>
2023-04-04 17:22:36 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
 # Create User
-sudo useradd testUser
+sudo useradd --no-create-home --shell /bin/false testUser
 
 # Add user to sudo group
 sudo usermod -aG sudo testUser
 
 # Copy the private key used to create remote machine
-scp /config testUser@54.160.160.122:/etc/ssh/sshd_config
+sudo mkdir /home/testUser/.ssh
+sudo chown -R testUser:testUser /home/testUser/.ssh
+sudo scp '/home/$USER/.ssh/authorized_keys' /home/testUser/.ssh/authorized_keys
 
-# Edit sshd_config file
-sudo mv /config /etc/ssh/sshd_config
+# Edit configuration for the sshd_config file
+scp /config testUser@54.160.160.122:/etc/ssh/sshd_config
 
 # Restart sshd service
 sudo systemctl restart sshd
