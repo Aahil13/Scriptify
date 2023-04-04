d6bf16b2f6b65bdc59d671160c327610bd94ff64
onyeanuna prince eze <onyeanunaprince@gmail.com>
2023-04-04 17:40:33 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 # Create User
-sudo useradd testUser -p testUser -m
+sudo useradd testUser -m
+
+# Set password for new user
+sudo passwd testUser
 
 # Add user to sudo group
 sudo usermod -aG sudo testUser
